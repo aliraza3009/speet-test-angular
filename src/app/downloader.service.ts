1b793f9b2e6d6cbb8c3eb78f986cd86c02e0fdb5
aliraza3009 <aliraza3009@gmail.com>
2022-07-04 20:06:22 +0400

    added cache control headers to httprequest

--- src/app/downloader.service.ts
+++ src/app/downloader.service.ts
@@ -5,11 +5,17 @@ import { HttpClient, HttpRequest } from '@angular/common/http';
   providedIn: 'root',
 })
 export class DownloaderService {
-  fileUrl: string = 'assets/testfile.dat?v=' + new Date().getTime();
+  fileUrl: string = 'assets/testfile.dat';
   constructor(private http: HttpClient) {}
 
   downloadFile() {
     const request = new HttpRequest('GET', this.fileUrl, {
+      headers: new Headers({
+        'Cache-Control':
+          'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
+        Pragma: 'no-cache',
+        Expires: '0',
+      }),
       reportProgress: true,
       responseType: 'blob',
     });
